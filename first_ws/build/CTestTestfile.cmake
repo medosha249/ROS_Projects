910c264647968173c3141248f5dcd029aa5c6bc5
medosha249 <medosha249@gmail.com>
2024-10-11 19:19:21 +0300

    Update

--- first_ws/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/alashkar/Desktop/Github/ROS_Projects/first_ws/src
-# Build directory: /home/alashkar/Desktop/Github/ROS_Projects/first_ws/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("basic_pkg")
