8710b6b33eddca3ca8185d5ceb398753965391c3
medosha249 <medosha249@gmail.com>
2025-02-01 02:28:47 +0200

    Auto Navigation Update

---
+++ scratch_ws/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/alashkar/Desktop/ROS_Projects/scratch_ws/devel:$CMAKE_PREFIX_PATH"
+export PWD='/home/alashkar/Desktop/ROS_Projects/scratch_ws/build'
+export ROSLISP_PACKAGE_DIRECTORIES="/home/alashkar/Desktop/ROS_Projects/scratch_ws/devel/share/common-lisp:$ROSLISP_PACKAGE_DIRECTORIES"
+export ROS_PACKAGE_PATH="/home/alashkar/Desktop/ROS_Projects/scratch_ws/src:$ROS_PACKAGE_PATH"
---
+++ scratch_ws/build/navigation_pkg/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/alashkar/Desktop/ROS_Projects/scratch_ws/src/navigation_pkg
+# Build directory: /home/alashkar/Desktop/ROS_Projects/scratch_ws/build/navigation_pkg
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
