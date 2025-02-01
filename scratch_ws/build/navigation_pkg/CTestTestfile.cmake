ca01ec6f46a4e8fb2bc763b29421947ed1f34efa
medosha249 <medosha249@gmail.com>
2025-02-01 02:29:19 +0200

    Auto Navigation Update

--- scratch_ws/build/navigation_pkg/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/alashkar/Desktop/ROS_Projects/scratch_ws/src/navigation_pkg
-# Build directory: /home/alashkar/Desktop/ROS_Projects/scratch_ws/build/navigation_pkg
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
